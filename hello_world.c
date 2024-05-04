754f25a3af62ed3e9ede97f4ef9de166e961b84c
yujinzhu1 <168119646+yujinzhu1@users.noreply.github.com>
2024-05-04 16:53:44 +0100

    Add files via upload

---
+++ hello_world.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(void)   // function which is run when the programs starts 
+
+{ 
+
+printf("Hello. My email address is:\nyujin.zhu@student.manchester.ac.uk"); // our statement/command to run 
+
+    return 0; // the return statement for the function main 
+
+}
